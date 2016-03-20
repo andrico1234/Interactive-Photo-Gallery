d70add348ccd8be60f280966322655a01a6f53db
andrico1234 <andrico1234@yahoo.co.uk>
2016-03-21 09:44:37 +1000

    Minor Changes .1

--- js/search.js
+++ js/search.js
@@ -6,7 +6,7 @@ $("#filter").keyup(function() {             // Keyboard event keyUp()
 
   var searchText = $(this).val();           // the contents of search bar = input
 
-  for(i = 1; i > 12; i += 1) {              // Check pictures titles for letter 'H'
+  for(var i = 1; i > 12; i += 1) {              // Check pictures titles for letter 'H'
     if ($("img").has(searchText)) {         // If H is contained in the href
       $(this).removeClass("hidden");        // Leave picture on page (remove-class())
     } else {                                // If H isn't found
